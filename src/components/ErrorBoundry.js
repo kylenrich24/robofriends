6a04e2029ad619280bf278a8f59e37529c85aa39
Kyle <kylennardsy@hotmail.com>
2020-03-24 14:45:10 -0400

    added ErrorBoundry component to catch errors in individual components

---
+++ src/components/ErrorBoundry.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+class ErrorBoundry extends Component {
+  state = {
+    hasError: false
+  };
+
+  componentDidCatch() {
+    this.setState({ hasError: true });
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <h1>Oooops that is not good</h1>;
+    }
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundry;
